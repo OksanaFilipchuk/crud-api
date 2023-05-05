3e3dcf58da0011663706facd6430f5573c19ccaa
Oksana Filipchuk <oksanafilipchuk@MacBook-Air-Oksana.local>
2023-05-05 13:39:37 +0300

    feat: add put method

--- src/methods/get-request.ts
+++ src/methods/get-request.ts
@@ -1,28 +1,6 @@
 import { ServerResponse, IncomingMessage } from "http";
-
-import { readFile } from "fs/promises";
-import path from "path";
-
-interface User {
-  id: string;
-  username: string;
-  age: number;
-  hobbies: string[];
-}
-
-async function getUsers() {
-  const db = path.resolve("./src", "db.json");
-  const users = JSON.parse((await readFile(db)).toString()).users;
-  return users;
-}
-
-async function getSingleUser(id: string) {
-  const db = path.resolve("./src", "db.json");
-  const result = await readFile(db);
-  const obj = await JSON.parse(result.toString());
-  const user = obj.users.filter((el: User) => el.id === id)[0];
-  return user;
-}
+import { getUsers } from "../utils.js";
+import { getSingleUser } from "../utils.js";
 
 export async function getRequest(
   request: IncomingMessage,
@@ -33,8 +11,8 @@ export async function getRequest(
     /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
   );
 
-  if (request.url === "/api" || request.url === "/api") {
-    response.writeHead(200, { "Content-type": "text/html" });
+  if (request.url === "/api" || request.url === "/") {
+    response.writeHead(200, { "Content-type": "application/json" });
     response.write(JSON.stringify({ message: "Welcome to crud-api" }));
     response.end();
   } else if (
@@ -42,25 +20,25 @@ export async function getRequest(
     request.url?.split("/")[2] === "users" &&
     !userId
   ) {
-    response.writeHead(200, { "Content-type": "text/html" });
+    response.writeHead(200, { "Content-type": "application/json" });
     response.write(JSON.stringify(await getUsers(), null, 2));
     response.end();
   } else if (userId && userId.match(v4)) {
     if (await getSingleUser(userId)) {
-      response.writeHead(200, { "Content-type": "text/html" });
+      response.writeHead(200, { "Content-type": "application/json" });
       response.write(JSON.stringify(await getSingleUser(userId), null, 2));
       response.end();
     } else {
-      response.writeHead(404, { "Content-type": "text/html" });
+      response.writeHead(404, { "Content-type": "application/json" });
       response.end(
         JSON.stringify({ message: `User with id ${userId} doesn't exist` })
       );
     }
   } else if (userId) {
-    response.writeHead(400, { "Content-type": "text/html" });
+    response.writeHead(400, { "Content-type": "application/json" });
     response.end(JSON.stringify({ message: `Inavlid user id` }));
   } else {
-    response.writeHead(404, { "Content-type": "text/html" });
+    response.writeHead(404, { "Content-type": "application/json" });
     response.end(JSON.stringify({ message: `Page not found` }));
   }
 }
